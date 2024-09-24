101cfb776a82730f4bf59f34d0e8a834d6ecfa9b
Paulo Bruno Batista de Sena <paulo.sena@tce.am.gov.br>
2024-09-24 10:00:23 -0400

    refactoring item club

--- lib/app/modules/home/pages/clubes_page.dart
+++ lib/app/modules/home/pages/clubes_page.dart
@@ -7,35 +7,37 @@ class ClubesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        padding: const EdgeInsets.all(8.0),
-        children: const [
-          ClubItem(
-            nome: 'Ursinhos',
-            imagem: 'assets/icon_clubs/ursinhos.png',
-          ),
-          ClubItem(
-            nome: 'Faísca',
-            imagem: 'assets/icon_clubs/faisca.png',
-          ),
-          ClubItem(
-            nome: 'Flama',
-            imagem: 'assets/icon_clubs/flama.png',
-          ),
-          ClubItem(
-            nome: 'Tocha',
-            imagem: 'assets/icon_clubs/tocha.png',
-          ),
-          ClubItem(
-            nome: 'Jovens Vencedores',
-            imagem: 'assets/icon_clubs/jovens_vencedores.png',
-          ),
-          ClubItem(
-            nome: 'VQ7',
-            imagem: 'assets/icon_clubs/vq7.png',
-          ),
-        ],
+    return const Scaffold(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            ClubItem(
+              nome: 'Ursinhos',
+              imagem: 'assets/icon_clubs/ursinhos.png',
+            ),
+            ClubItem(
+              nome: 'Faísca',
+              imagem: 'assets/icon_clubs/faisca.png',
+            ),
+            ClubItem(
+              nome: 'Flama',
+              imagem: 'assets/icon_clubs/flama.png',
+            ),
+            ClubItem(
+              nome: 'Tocha',
+              imagem: 'assets/icon_clubs/tocha.png',
+            ),
+            ClubItem(
+              nome: 'Jovens Vencedores',
+              imagem: 'assets/icon_clubs/jovens_vencedores.png',
+            ),
+            ClubItem(
+              nome: 'VQ7',
+              imagem: 'assets/icon_clubs/vq7.png',
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/app/modules/home/widgets/club_item.dart
+++ lib/app/modules/home/widgets/club_item.dart
@@ -9,9 +9,16 @@ class ClubItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      elevation: 0,
+      color: Colors.grey[100],
       child: ListTile(
+        contentPadding:
+            const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
         leading: Image.asset(imagem, width: 50, height: 50),
-        title: Text(nome),
+        title: Text(
+          nome,
+          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
