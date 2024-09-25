0ceed863d76e9defdbef7ace6cbd52d04e1d572d
Paulo Sena <paulobruno.sena@gmail.com>
2024-09-25 17:23:10 -0400

    criada page de jogos

--- lib/app/modules/home/pages/jogos_page.dart
+++ lib/app/modules/home/pages/jogos_page.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 
+import '../../../core/global/clubs_enum.dart';
+import '../widgets/club_item.dart';
+
 class JogosPage extends StatelessWidget {
   const JogosPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text('Jogos Page'),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            ClubItem(club: ClubsEnum.ursinhos),
+            ClubItem(club: ClubsEnum.faisca),
+            ClubItem(club: ClubsEnum.flama),
+            ClubItem(club: ClubsEnum.tocha),
+            ClubItem(club: ClubsEnum.jv),
+            ClubItem(club: ClubsEnum.vq7),
+          ],
+        ),
       ),
     );
   }
